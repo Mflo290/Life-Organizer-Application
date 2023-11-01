5a69a6797ae6e0240295f82bc72c8ad562576995
Mflo290 <marioflo455@gmail.com>
2023-10-31 22:25:50 -0700

    Created and designed the sign in user interface.

---
+++ app/src/main/java/com/example/lifeorganizerapp/UI/HomeScreen.java
@@ -0,0 +1,16 @@
+package com.example.lifeorganizerapp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lifeorganizerapp.R;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
---
+++ app/src/main/java/com/example/lifeorganizerapp/UI/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.lifeorganizerapp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.lifeorganizerapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize Variables
+        Button loginButton = findViewById(R.id.loginButton);
+
+        //OnClickListener for login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
