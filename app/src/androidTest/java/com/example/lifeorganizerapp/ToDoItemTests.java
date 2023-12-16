9fc13b52fe48253fac3d67de24cdfb9e0bae7094
Mflo290 <marioflo455@gmail.com>
2023-12-15 22:02:00 -0800

    Finishing up testing

---
+++ app/src/androidTest/java/com/example/lifeorganizerapp/ToDoItemTests.java
@@ -0,0 +1,115 @@
+package com.example.lifeorganizerapp;
+
+
+import static org.junit.Assert.assertTrue;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.remote.EspressoRemoteMessage;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.lifeorganizerapp.dao.ItemDAO;
+import com.example.lifeorganizerapp.database.DatabaseBuilder;
+import com.example.lifeorganizerapp.entities.ToDoItem;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+@RunWith(AndroidJUnit4.class)
+public class ToDoItemTests {
+
+    private DatabaseBuilder itemDatabase;
+    private ItemDAO itemDAO;
+
+
+
+    @Before
+    public void setUp() throws Exception {
+
+        itemDatabase = Room.inMemoryDatabaseBuilder(
+                ApplicationProvider.getApplicationContext(),
+                DatabaseBuilder.class
+        ).build();
+        itemDAO = itemDatabase.itemDAO();
+    }
+
+//    @Test
+//    public void testGetCompleteedTasks() throws InterruptedException {
+//        // Insert some test items into the database
+//        ToDoItem testItem1 = new ToDoItem(0, "TestItem1", false, 1, "TestList");
+//        ToDoItem testItem2 = new ToDoItem(0, "TestItem2", true, 1, "TestList");
+//        ToDoItem testItem3 = new ToDoItem(0, "TestItem3", true, 2, "AnotherList");
+//        itemDAO.insert(testItem1);
+//        itemDAO.insert(testItem2);
+//        itemDAO.insert(testItem3);
+//
+//        // Observe the LiveData from getCompletedTasks()
+//        LiveData<List<ToDoItem>> completedTasksLiveData = itemDAO.getCompletedTasks();
+//        TestObserver<List<ToDoItem>> testObserver = new TestObserver<>();
+//        completedTasksLiveData.observeForever(testObserver);
+//
+//
+//
+//    }
+
+
+
+    @Test
+    public void testGetAssociatedItemsWithNullDate() throws InterruptedException {
+        // Insert some test items into the database
+        ToDoItem testItem1 = new ToDoItem(0, "TestItem1", false, 1, "TestList");
+        ToDoItem testItem2 = new ToDoItem(0, "TestItem2", false, 1, "TestList");
+        ToDoItem testItem3 = new ToDoItem(0, "TestItem3", false, 2, "AnotherList");
+        itemDAO.insert(testItem1);
+        itemDAO.insert(testItem2);
+        itemDAO.insert(testItem3);
+
+
+        // Get associated items with a null date completed for list ID 1
+        List<ToDoItem> associatedItems = itemDAO.getAssociatedItemsWithNullDateCompleted(1);
+
+        // Ensure the retrieved list contains items associated with list ID 1 and have null date completed
+        boolean found = false;
+        for (ToDoItem item : associatedItems) {
+            if (item.getListID() == 1 && item.getDateCompleted() == null) {
+                found = true;
+                break;
+            }
+        }
+        assertTrue(found);
+    }
+
+
+    @Test
+    public void afterInsertItem() {
+        ToDoItem testItem = new ToDoItem(0, "TestItem", false, 1, "TestList");
+        itemDAO.insert(testItem);
+
+        // Get all items from the DAO
+        List<ToDoItem> allToDoItems = itemDAO.getAllToDoItems();
+
+        // Ensure the item is inserted by checking if an equivalent item is in the list of returned items
+        boolean found = false;
+        for (ToDoItem item: allToDoItems) {
+            if(item.getTitle().equals(testItem.getTitle()) &&
+                    item.isChecked() == testItem.isChecked() &&
+                    item.getListID() == testItem.getListID() &&
+                    item.getListName().equals(testItem.getListName())) {
+                found = true;
+                break;
+            }
+        }
+        assertTrue(found);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        itemDatabase.close();
+    }
+}
